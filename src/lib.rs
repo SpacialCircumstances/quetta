62dfa5a1229d83539f0e6f8800f03bae146fc6d1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-06 23:28:41 +0200

    Implement debug and display

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::rc::Rc;
+use std::fmt::{Debug, Formatter, Display};
+use std::borrow::Borrow;
 
 #[derive(Clone)]
 struct IString(Rc<str>);
@@ -14,6 +16,32 @@ pub struct Text(TextData);
 impl Clone for Text {
     fn clone(&self) -> Self {
         Self(self.0.clone())
+    }
+}
+
+impl Debug for Text {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s: &str = self.into();
+        write!(f, "{}", s)
+    }
+}
+
+impl Display for Text {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s: &str = self.into();
+        write!(f, "{}", s)
+    }
+}
+
+impl<'a> From<&'a Text> for &'a str {
+    fn from(t: &'a Text) -> Self {
+        match &t.0 {
+            TextData::Entire(s) => &*s.0,
+            TextData::Slice { string, start, len } => {
+                let s = &*string.0;
+                &s[*start..*start+*len]
+            }
+        }
     }
 }
 
